f0837561bd06928acab064635e3b57a7aed41109
Alan Read <adr4069@g.rit.edu>
2019-12-17 16:38:00 -0500

    Completed sign-in ui

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require ('express');
+const path = require('path');
 
 const app = express();
-app.get('/', function (req, res) {
-    res.send('hello');
 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname + '/public/')));
+
+app.get('/', function (req, res) {
+    res.render('home')
 })
 
 app.listen(3000, function () {
